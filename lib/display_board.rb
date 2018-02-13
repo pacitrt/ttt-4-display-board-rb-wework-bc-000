b31a9fba63ecd4ffe85d6fe18968428773b91d4b
Rio Pacit <rio.pacit@wework.com>
2018-02-13 17:55:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,15 +2,16 @@
 # out the current state.
 
 board = [" "," "," "," "," "," "," "," "," "]
-output = capture_puts{ display_board(board) }
-rows = output.split("\3")
+
 
 def display_board(board)
-    puts rows[0] = " #{board[0]} | #{board[1]} | #{board[2]}"
+  output = capture_puts{ display_board(board) }
+  rows = output.split("\3")
+    puts rows[0] = "   |   |   "
     puts rows[1] = "-----------"
-    puts rows[2] = " #{board[3]} | #{board[4]} | #{board[5]}"
+    puts rows[2] = "   |   |   "
     puts rows[3] = "-----------"
-    puts rows[4] = " #{board[6]} | #{board[7]} | #{board[8]}"
+    puts rows[4] = "   |   |   "
 end
 puts display_board(board)
 
