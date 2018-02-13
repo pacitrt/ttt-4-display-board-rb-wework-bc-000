c26723084baf5bc71222d9bd22b12ccd2aba1fcb
Rio Pacit <rio.pacit@wework.com>
2018-02-13 17:39:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
 def display_board(board)
 
