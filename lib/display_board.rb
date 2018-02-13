978f14ddccd74c9a86657d117eb59b5f7dda3b1c
Rio Pacit <rio.pacit@wework.com>
2018-02-13 17:51:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 board = [" "," "," "," "," "," "," "," "," "]
-
+output = capture_puts{ display_board(board) }
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]}" 
     puts "-----------"
